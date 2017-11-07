4ed0e8f96b2e3e3b9836b048ab356ec036e088b3
krieg2 <aaronk1@yahoo.com>
2017-11-06 19:19:14 -0500

    here are 2 servers

---
+++ server.js
@@ -0,0 +1,38 @@
+// Require/import the HTTP module
+var http = require("http");
+
+// Define a port to listen for incoming requests
+var PORT1 = 7000;
+var PORT2 = 7500;
+
+// Create a generic function to handle requests and responses
+function handleRequestGood(request, response) {
+
+  // Send the below string to the client when the user visits the PORT URL
+  response.end("Your code is perfect.");
+}
+
+function handleRequestBad(request, response) {
+
+  // Send the below string to the client when the user visits the PORT URL
+  response.end("Your code is horrible.");
+}
+
+// Use the Node HTTP package to create our server.
+// Pass the handleRequest function to empower it with functionality.
+var server1 = http.createServer(handleRequestGood);
+var server2 = http.createServer(handleRequestBad);
+
+// Start our server so that it can begin listening to client requests.
+server1.listen(PORT1, function() {
+
+  // Log (server-side) when our server has started
+  console.log("Server listening on: http://localhost:" + PORT1);
+});
+
+
+server2.listen(PORT2, function() {
+
+  // Log (server-side) when our server has started
+  console.log("Server listening on: http://localhost:" + PORT2);
+});
